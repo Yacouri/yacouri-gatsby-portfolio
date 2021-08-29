7df67a14d0b063aaddf4fae76686786fe1ee6919
Yacouri <zouhiryac1@gmail.com>
2021-08-30 00:44:15 +0100

    change config for deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         background_color: '#f7f0eb',
         theme_color: '#a2466c',
         display: 'standalone',
-        icon: './src/images/logo.png',
+        icon: 'src/images/logo.png',
       },
     },
     {
